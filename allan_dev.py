dd2510762e7e67ae5480ef3e1ba2b0ebd9d09c96
Tim Lawson <tim.lawson@callaghaninnovation.govt.nz>
2026-04-07 09:53:05 +1200

    Added allan_dev.py (was developed outside this repo but now included).
    The remove outliers part now defaults to retaining all data (no cuts), rather than cutting anything beyond 3 std devs.

---
+++ allan_dev.py
@@ -0,0 +1,82 @@
+import os
+import statistics
+
+"""
+allan_dev.py
+
+This script takes CCC bvd.txt data files and outputs a .dat file, suitable as input to
+Stable32 which is used to generate Allan deviation plots of the data.
+"""
+
+TIMEBASE = 0.02  # One 50 Hz cycle.
+
+folder = input('Path to CCC BVD file: ')  # E.g. ...\Magnicon CCC\Commissioning\Data\2024-08-15_CCC
+filename = input('BVD file name: ')  # E.g. 240815_002_1628_bvd.txt
+basename = filename.split('.')[0]
+print(f'basename: "{basename}"')
+dir_filename = os.path.join(folder, filename)
+print(dir_filename)
+
+"""
+Open bvd file and read values into a list (ignoring header).
+"""
+bvd_vals = []
+with open(dir_filename, 'r') as bvd_file:
+    line = bvd_file.readline()  # 1st line
+    while line[0] != '#':  # Header line before 1st data row (on exit)
+        line = bvd_file.readline()
+        if line.startswith('integration time'):
+            sampletime = TIMEBASE*float(line.split(':')[1].strip())  # in s
+        if line.startswith('number of samples per half cycle'):
+            samples_per_cycle = 2*float(line.split(':')[1].strip())
+            cycletime = samples_per_cycle*sampletime
+
+    while True:
+        line = bvd_file.readline()  # data row
+        if not line:
+            break  # break at EOF
+        bvd = float(line.split()[1])  # Split at " ", cast 2nd item as float
+        bvd_vals.append(bvd)
+print(f'End of file - read {len(bvd_vals)} values.\n')
+
+"""
+Truncation cut: remove 'dead' points from end of file, in case of SQUID-lock-loss
+"""
+bvd_vals_trunc = []
+n_keep = input("Enter cut-off point (default is to keep all points): ")
+if n_keep == "":
+    n_keep = len(bvd_vals)
+n_keep = int(n_keep)
+bvd_vals_trunc = bvd_vals[0:n_keep-1]
+av_value_trunc = statistics.mean(bvd_vals_trunc)
+print(f'Cut {len(bvd_vals) - n_keep} points. Mean bvd (truncated) = {av_value_trunc:.2e} V')
+
+"""
+Data quality cut: remove outliers.
+"""
+bvd_vals_kept = []
+n_sd = input("No. of std dev's for data cut (default is no cut): ")
+if n_sd == "":  # Default is to keep all data
+    bvd_vals_kept = bvd_vals_trunc
+    # n_sd = 3  # ~99.7% kept (if normal distribution).
+else:
+    sdn = float(n_sd)*statistics.stdev(bvd_vals_trunc)
+    for val in bvd_vals_trunc:
+        if (val > av_value_trunc + sdn) or (val < av_value_trunc - sdn):
+            continue  # skip failed points
+        bvd_vals_kept.append(val)
+    print(f'mean bvd = {av_value_trunc:.2e} +/- {sdn:.2e} V')
+
+print(f'Retained {len(bvd_vals_kept)} values with mean value {statistics.mean(bvd_vals_kept):.2e}')
+
+"""
+Write out a 'cleaned' version of the data
+"""
+outfilename = f'{basename}.dat'
+dir_filename = os.path.join(folder, outfilename)
+with open(dir_filename, 'w') as dat_file:
+    cycle_count = 0
+    for val in bvd_vals_kept:
+        cycle_count += 1
+        line = f'{cycle_count*cycletime/86400:.6f}\t{val}\n'  # N*cycles, expressed in days.
+        dat_file.write(line)
